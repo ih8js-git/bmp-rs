6a2bb35a1d3e492f8d1d94e7e5bbd90e37bfaf6d
Lex <lex.bruynooghe@gmail.com>
2026-06-13 11:05:48 +0200

    moved getters to operations module, used enums instead of raw numbers

--- src/card/parse.rs
+++ src/card/parse.rs
@@ -1,140 +1,79 @@
-use crate::card::{Card, Enhancement, Rank, Seal, Suit};
+use crate::card::{Card, Rank, Suit, Edition, Enhancement, Seal, get_card_rank, get_card_suit, get_card_edition, get_card_enhancement, get_card_seal};
 
-pub fn get_card_rank(card: &Card) -> Rank {
-    match card.meta >> 12 {
-        0 => Rank::Two,
-        1 => Rank::Three,
-        2 => Rank::Four,
-        3 => Rank::Five,
-        4 => Rank::Six,
-        5 => Rank::Seven,
-        6 => Rank::Eight,
-        7 => Rank::Nine,
-        8 => Rank::Ten,
-        9 => Rank::Jack,
-        10 => Rank::Queen,
-        11 => Rank::King,
-        12 => Rank::Ace,
-        _ => panic!("Invalid rank"),
-    }
-}
-
-pub fn get_card_suit(card: &Card) -> Suit {
-    let suit = (card.meta & 0b11_000_0000_000) >> 10;
-    match suit {
-        0 => Suit::Spades,
-        1 => Suit::Hearts,
-        2 => Suit::Clubs,
-        3 => Suit::Diamonds,
-        _ => panic!("Invalid suit"),
-    }
-}
-
-pub fn get_card_enhancement(card: &Card) -> Enhancement {
-    let enhancement = (card.meta & 0b1111_000) >> 3;
-    match enhancement {
-        0 => Enhancement::None,
-        1 => Enhancement::Bonus,
-        2 => Enhancement::Mult,
-        3 => Enhancement::Wild,
-        4 => Enhancement::Glass,
-        5 => Enhancement::Steel,
-        6 => Enhancement::Stone,
-        7 => Enhancement::Gold,
-        8 => Enhancement::Lucky,
-        _ => panic!("Invalid enhancement"),
-    }
-}
-
-pub fn get_card_seal(card: &Card) -> Seal {
-    let seal = card.meta & 0b111;
-    match seal {
-        0 => Seal::None,
-        1 => Seal::Gold,
-        2 => Seal::Red,
-        3 => Seal::Blue,
-        4 => Seal::Purple,
-        _ => panic!("Invalid seal"),
-    }
-}
 pub fn parse_card_to_text(card: &Card) -> String {
-    let rank = card.meta >> 12;
-    let suit = (card.meta & 0b11_000_0000_000) >> 10;
-    let edition = (card.meta & 0b111_000_000) >> 7;
-    let enhancement = (card.meta & 0b1111_000) >> 3;
-    let seal = card.meta & 0b111;
+    let rank = get_card_rank(card);
+    let suit = get_card_suit(card);
+    let edition = get_card_edition(card);
+    let enhancement = get_card_enhancement(card);
+    let seal = get_card_seal(card);
     return format!(
         "{} {} {} {} of {}",
-        parse_card_seal_to_text(seal as u8),
-        parse_card_edition_to_text(edition as u8),
-        parse_card_enhancement_to_text(enhancement as u8),
-        parse_card_rank_to_text(rank as u8),
-        parse_card_suit_to_text(suit as u8),
+        parse_card_seal_to_text(seal),
+        parse_card_edition_to_text(edition),
+        parse_card_enhancement_to_text(enhancement),
+        parse_card_rank_to_text(rank),
+        parse_card_suit_to_text(suit),
     );
 }
 
-pub fn parse_card_rank_to_text(rank: u8) -> String {
+pub fn parse_card_rank_to_text(rank: Rank) -> String {
     match rank {
-        0 => "2".to_string(),
-        1 => "3".to_string(),
-        2 => "4".to_string(),
-        3 => "5".to_string(),
-        4 => "6".to_string(),
-        5 => "7".to_string(),
-        6 => "8".to_string(),
-        7 => "9".to_string(),
-        8 => "10".to_string(),
-        9 => "J".to_string(),
-        10 => "Q".to_string(),
-        11 => "K".to_string(),
-        12 => "A".to_string(),
-        _ => "Error".to_string(),
+        Rank::Two => "2".to_string(),
+        Rank::Three => "3".to_string(),
+        Rank::Four => "4".to_string(),
+        Rank::Five => "5".to_string(),
+        Rank::Six => "6".to_string(),
+        Rank::Seven => "7".to_string(),
+        Rank::Eight => "8".to_string(),
+        Rank::Nine => "9".to_string(),
+        Rank::Ten => "10".to_string(),
+        Rank::Jack => "J".to_string(),
+        Rank::Queen => "Q".to_string(),
+        Rank::King => "K".to_string(),
+        Rank::Ace => "A".to_string(),
     }
 }
 
-pub fn parse_card_suit_to_text(suit: u8) -> String {
+pub fn parse_card_suit_to_text(suit: Suit) -> String {
     match suit {
-        0 => "Spades".to_string(),
-        1 => "Hearts".to_string(),
-        2 => "Clubs".to_string(),
-        3 => "Diamonds".to_string(),
-        _ => "Error".to_string(),
+        Suit::Spades => "Spades".to_string(),
+        Suit::Hearts => "Hearts".to_string(),
+        Suit::Clubs => "Clubs".to_string(),
+        Suit::Diamonds => "Diamonds".to_string(),
     }
 }
 
-pub fn parse_card_edition_to_text(edition: u8) -> String {
+pub fn parse_card_edition_to_text(edition: Edition) -> String {
     match edition {
-        0 => "None".to_string(),
-        1 => "Foil".to_string(),
-        2 => "Holographic".to_string(),
-        3 => "Polychrome".to_string(),
-        4 => "Negative".to_string(),
-        _ => "Error".to_string(),
+        Edition::None => "None".to_string(),
+        Edition::Foil => "Foil".to_string(),
+        Edition::Holographic => "Holographic".to_string(),
+        Edition::Polychrome => "Polychrome".to_string(),
+        Edition::Negative => "Negative".to_string(),
     }
 }
 
-pub fn parse_card_enhancement_to_text(enhancement: u8) -> String {
+pub fn parse_card_enhancement_to_text(enhancement: Enhancement) -> String {
     match enhancement {
-        0 => "None".to_string(),
-        1 => "Bonus".to_string(),
-        2 => "Mult".to_string(),
-        3 => "Wild".to_string(),
-        4 => "Glass".to_string(),
-        5 => "Steel".to_string(),
-        6 => "Stone".to_string(),
-        7 => "Gold".to_string(),
-        8 => "Lucky".to_string(),
-        _ => "Error".to_string(),
+        Enhancement::None => "None".to_string(),
+        Enhancement::Bonus => "Bonus".to_string(),
+        Enhancement::Mult => "Mult".to_string(),
+        Enhancement::Wild => "Wild".to_string(),
+        Enhancement::Glass => "Glass".to_string(),
+        Enhancement::Steel => "Steel".to_string(),
+        Enhancement::Stone => "Stone".to_string(),
+        Enhancement::Gold => "Gold".to_string(),
+        Enhancement::Lucky => "Lucky".to_string(),
     }
 }
 
-pub fn parse_card_seal_to_text(seal: u8) -> String {
+pub fn parse_card_seal_to_text(seal: Seal) -> String {
     match seal {
-        0 => "None".to_string(),
-        1 => "Red Seal".to_string(),
-        2 => "Blue Seal".to_string(),
-        3 => "Gold Seal".to_string(),
-        4 => "Purple Seal".to_string(),
+        Seal::None => "None".to_string(),
+        Seal::Red => "Red Seal".to_string(),
+        Seal::Blue => "Blue Seal".to_string(),
+        Seal::Gold => "Gold Seal".to_string(),
+        Seal::Purple => "Purple Seal".to_string(),
         _ => "Error".to_string(),
     }
 }
