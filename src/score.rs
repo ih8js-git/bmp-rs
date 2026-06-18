757b10fcfbf0e9417f6cbd04670cfd3f9d2b3936
Lex <lex.bruynooghe@gmail.com>
2026-06-18 18:49:18 +0200

    cargo fmt

--- src/score.rs
+++ src/score.rs
@@ -269,7 +269,7 @@ pub fn get_hand_type(hand: &[Card], jokers: &[Joker]) -> (Hand, Vec<usize>) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::card::core::{create_test_card, Enhancement, Rank, Suit};
+    use crate::card::core::{Enhancement, Rank, Suit, create_test_card};
     use crate::card::operations::set_card_enhancement;
 
     fn get_wild(rank: Rank, suit: Suit) -> Card {
