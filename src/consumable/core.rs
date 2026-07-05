457de98aa9bdac21103a7a5a97eafc1cf2960426
Lex <lex.bruynooghe@gmail.com>
2026-07-06 01:49:14 +0200

    simple consumable played to delta logic for planets

--- src/consumable/core.rs
+++ src/consumable/core.rs
@@ -1,4 +1,6 @@
-use crate::consumable::{Planet, Spectral, Tarot};
+use crate::consumable::{Planet, Spectral, Tarot, use_planet_to_deltas};
+use crate::game::delta::GameDelta;
+use crate::game::state::GameState;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 #[repr(u8)]
@@ -13,6 +15,16 @@ pub struct ConsumableState {
     pub consumable: Consumable,
     pub added_sell_value: u8,
     pub is_negative: bool,
+}
+
+pub fn simple_consumable_action_to_deltas(consumable_idx: u16, gs: &GameState) -> Vec<GameDelta> {
+    let consumable_state = &gs.consumables[consumable_idx as usize];
+
+    match consumable_state.consumable {
+        Consumable::Planet(p) => use_planet_to_deltas(p, gs),
+        Consumable::Tarot(t) => Vec::new(),    // TODO
+        Consumable::Spectral(s) => Vec::new(), //TODO
+    }
 }
 
 pub fn create_tarot_consumable(t: Tarot) -> ConsumableState {
