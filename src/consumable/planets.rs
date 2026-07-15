ccefeda5faa37a8d760aadb53d77121eade28ac9
Lex <lex.bruynooghe@gmail.com>
2026-07-16 00:08:30 +0200

    using a planet now notifies listening jokers, which will add deltas to the result

--- src/consumable/planets.rs
+++ src/consumable/planets.rs
@@ -1,8 +1,9 @@
 use crate::consumable::core::Consumable;
-use crate::events::{JokerUpdateEvent, event_to_bitmask, notify_jokers};
+use crate::events::notify_jokers;
 use crate::game::action::GameAction;
 use crate::game::delta::GameDelta;
 use crate::game::state::GameState;
+use crate::joker::fn_arrays::planet::PLANET_FN_IDX;
 use strum_macros::Display;
 
 #[derive(Debug, Copy, Clone, PartialEq, Display)]
@@ -29,8 +30,8 @@ pub fn use_planet_to_deltas(p: Planet, game_state: &GameState) -> Vec<GameDelta>
     res.push(GameDelta::Planet { planet: p, diff: 1 });
 
     // notify listening jokers
-    if game_state.joker_event_listener_amount[Us as usize] > 0 {
-        notify_jokers(&mut res, GameAction::UsePlanet, game_state);
+    if game_state.action_subscribed_amount[PLANET_FN_IDX] > 0 {
+        notify_jokers(&mut res, GameAction::UsePlanet { idx: 0 }, game_state); // planet location irrelivant
     }
 
     res
